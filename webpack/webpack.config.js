b8f5f719cc676579fa76d35d94be8dc5a8c509bf
Carney.Benjamin <carneybe@CSEAL-M101960.local>
2018-02-06 11:00:33 -0800

    Adds webpack-dev-server

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,7 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Slot Prototype'
+    }),
+    new CleanWebpackPlugin(['dist']),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
