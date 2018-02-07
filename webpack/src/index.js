9d66a401191d1c73e56dad72483155bd9970b7a1
Carney.Benjamin <carneybe@CSEAL-M101960.local>
2018-02-06 20:13:48 -0800

    Ports assetloader w/ image handling

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,9 +1,36 @@
-function component() {
-  var element = document.createElement('div');
+import * as PIXI from 'pixi.js';
+import './style.css';
 
-  element.innerHTML = ['Hello', 'webpack'].join(' ');
+//Create a Pixi Application
+let app = new PIXI.Application({
+    width: 1024,
+    height: 768,
+    antialiasing: true,
+    transparent: false,
+    resolution: 1
+});
 
-  return element;
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+//load a JSON file and run the `setup` function when it's done
+PIXI.loader
+.add([
+    './images/slottemplate_slot1.json',
+    "./images/slottemplate_slot2.json",
+    "./images/slottemplate_slot3.json",
+    "./images/slottemplate_slot4.json",
+    "./images/slottemplate_slot5.json",
+    "./images/slottemplate_slot6.json",
+    "./images/slottemplate_slot7.json",
+    "./images/slottemplate_common1.json",
+    "./images/slottemplate_paytable1.json",
+])
+.load(setup);
+
+function setup() {
+
+    let bg = PIXI.Sprite.fromFrame("bg_slot");
+    app.stage.addChild(bg);
+
 }
-
-document.body.appendChild(component());
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist'
   },
@@ -11,10 +13,24 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Slot Prototype'
     }),
-    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([
+      { from: 'src/assets' }
+    ]),
+    //new CleanWebpackPlugin(['dist']),
   ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      }
+    ]
   }
 };
